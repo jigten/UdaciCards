4162b6bdc3405a946fcf8bc32c3d0c1ab7e33402
Ugyen Jigten <ujigten@gmail.com>
2017-10-16 01:45:02 +0700

    feat; Add Redux and DeckList component

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
 import NewDeck from './components/NewDeck'
+import DeckList from './components/DeckList'
 
 export default class App extends React.Component {
   render() {
     return (
-      <NewDeck />
+      <Provider store={createStore(reducer)}>
+        <DeckList />
+      </Provider>
     );
   }
 }
--- components/DeckList.js
+++ components/DeckList.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { getDecks } from '../utils/api'
+import { connect } from 'react-redux'
+import { receiveDecks } from '../actions'
+import { AppLoading } from 'expo'
 
-export default class DeckList extends Component {
+class DeckList extends Component {
   state = {
-
+    ready: false
   }
 
   componentDidMount() {
-    
+    const { dispatch } = this.props
+
+    getDecks()
+      .then((decks) => dispatch(receiveDecks(decks)))
+      .then(() => this.setState(() => ({
+        ready: true
+      })))
   }
 
   render() {
-    return (
+    const { decks } = this.props
+    const { ready } = this.state
 
+    console.log(this.props)
+
+    if(ready === false) {
+      return <AppLoading />
+    }
+
+    return (
+      <View>
+        <Text>Done!</Text>
+      </View>
     )
   }
 }
@@ -21,3 +41,11 @@ export default class DeckList extends Component {
 const styles = StyleSheet.create({
 
 })
+
+function mapStateToProps(decks) {
+  return {
+    decks
+  }
+}
+
+export default connect(mapStateToProps)(DeckList)
