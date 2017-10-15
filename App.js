9be9cf3b7dfe7608bd257cdc06a9be50b307045c
Ugyen Jigten <ujigten@gmail.com>
2017-10-16 02:42:45 +0700

    feat: Add Tab Navigation

--- App.js
+++ App.js
@@ -3,15 +3,52 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import { TabNavigator } from 'react-navigation'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import NewDeck from './components/NewDeck'
 import DeckList from './components/DeckList'
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards' size={30} color={tintColor} />
+    }
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards-outline' size={30} color={tintColor} />
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: 'yellow',
+    style: {
+      height: 56,
+      backgroundColor: 'white',
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <DeckList />
+        <Tabs />
       </Provider>
     );
   }
